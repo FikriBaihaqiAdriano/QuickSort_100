b1d2000c370d34d64d1c423b09186a0eabdfa812
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-31 15:24:11 +0700

    membuat langkah 2,3,4 dari quicksort

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -45,3 +45,12 @@ void q_short(int low, int high)
 	int pivot, i, j;
 	if (low > high)					//langkah 1
 		return;
+
+	//Partition the list into two parts:
+	//One containing elements less that or equal to pivot
+	//Outher conntainning elements greather than pivot 
+
+	pivot = arr[low];				//langkah 2
+
+	i = low + 1;					//langkah 3
+	j = high;						//langkah 4
